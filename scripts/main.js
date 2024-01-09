6e8106bf9c7f45e11d7cc4613591078bbe3d075b
B-Audrey <audrey@devzone.fr>
2024-01-09 15:47:20 +0100

    feature: style ajustement toggle

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ import {getData} from './utils/functions.js';
 import {cardTemplate} from './templates/card.js';
 import {displayCountRecipes} from './templates/recipes.js';
 
+
+// Display every cards and total count
 const totalData = await getData();
 totalData.forEach( (currentCard) => {
     const cardToDisplay = cardTemplate(currentCard)
